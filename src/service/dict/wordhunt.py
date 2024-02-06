461ad987d5d99c8a14e106d948cd83d3bfd21ee4
VageLO <maksgrem9@gmail.com>
2024-02-07 00:47:41 +0300

    Change: added fields (meaning, examples, word form)

--- src/service/dict/wordhunt.py
+++ src/service/dict/wordhunt.py
@@ -21,30 +21,44 @@ class Wordhunt(WebService):
         data = self.get_response(u'{0}{1}'.format(self._get_url(), self.quote_word))
         soup = parse_html(data)
         result = {
-            'example': 'test1',
-            'word_form': 'test1',
+            'meaning': '',
+            'example': '',
+            'word_form': '',
         }
        
-        element = soup.find('div', class_='')
-        if element:
+        meaning = soup.find('div', class_='t_inline_en')
+        result['meaning'] = meaning
 
-            elements = element.find_all('div', class_='entry-body__el')
-            header_found = False
-            for element in elements:
-                    senses = element.find_all('div', class_='pos-body')
+        examples = soup.find_all('p', class_='ex_o')
+        if examples:
+            result['example'] = '<ul>'
+            for example in examples[0:3]:
+                result['example'] += f'<li>{example.get_text()}</li>'
+            result['example'] += '</ul>'
 
-                    span_posgram = element.find('div', class_='posgram')
-                    pos_gram = (span_posgram.get_text() if span_posgram else '')
-
+        word_forms = soup.find_all('div', class_='word_form_block')
+        if word_forms:
+            for form in word_forms:
+                result['word_form'] += f'{form.get_text()}'
         return self.cache_this(result)
 
     @with_styles(need_wrap_css=True, cssfile='_cambridge.css')
     def _css(self, val):
         return val
 
+    @export('MEANING')
+    def fld_meaning(self):
+        val = self._get_field('meaning')
+        if val == None or val == '':
+            return ''
+        return self._css(val)
+    
     @export('EXAMPLE')
     def fld_example(self):
-        return self._fld_img('image')
+        val = self._get_field('example')
+        if val == None or val == '':
+            return ''
+        return self._css(val)
 
     @export('WORDFORM')
     def fld_wordform(self):
